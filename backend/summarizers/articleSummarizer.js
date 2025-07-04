1c23f0277c934b590dfde7590c5e331d30d7fd80
Harshdeep Singh <143889275+Harshdeep-Singh2004@users.noreply.github.com>
2025-07-04 08:49:50 +0530

    Update articleSummarizer.js

--- backend/summarizers/articleSummarizer.js
+++ backend/summarizers/articleSummarizer.js
@@ -4,7 +4,7 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 async function generateArticle (article, headline) {
     try {
         article = article.slice(0, 8000);
-        const model = genAI.getGenerativeModel({ model:"gemini-1.5-flash" });
+        const model = genAI.getGenerativeModel({ model:"gemini-2.0-flash" });
         const prompt = `Summarize the article in 250 words (use small paragraphs breakdown), if no article give summary on the basis of the headline (Do not mention headline) : ${headline} \n ${article}`;
         const result = await model.generateContent(prompt);
         let summary = result.response.text();
